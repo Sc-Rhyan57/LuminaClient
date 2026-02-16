ca2975fa37cdefdc919c9db6ecd8eceaa3a232a1
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-02-16 02:22:51 -0300

    Fix VoxelShapesSerializer - remove SerializableVoxelShape dependency

--- Protocol/bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v924/serializer/VoxelShapesSerializer_v924.java
+++ Protocol/bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v924/serializer/VoxelShapesSerializer_v924.java
@@ -42,15 +42,12 @@ public class VoxelShapesSerializer_v924 implements BedrockPacketSerializer<Voxel
                 buf2.writeByte(frame.getXSize());
                 buf2.writeByte(frame.getYSize());
                 buf2.writeByte(frame.getZSize());
-
                 helper.writeArray(buf2, frame.getStorage(), (buf3, value) -> buf3.writeByte(value));
             });
-
             helper.writeArray(buf, shape.getXCoordinates(), ByteBuf::writeFloatLE);
             helper.writeArray(buf, shape.getYCoordinates(), ByteBuf::writeFloatLE);
             helper.writeArray(buf, shape.getZCoordinates(), ByteBuf::writeFloatLE);
         });
-
         VarInts.writeUnsignedInt(buffer, packet.getNameMap().size());
         packet.getNameMap().forEach((k, v) -> {
             helper.writeString(buffer, k);
@@ -61,42 +58,30 @@ public class VoxelShapesSerializer_v924 implements BedrockPacketSerializer<Voxel
     @Override
     public void deserialize(ByteBuf buffer, BedrockCodecHelper helper, VoxelShapesPacket packet) {
         List<VoxelShape> shapes = new ArrayList<>();
-
         helper.readArray(buffer, shapes, (buf, h) -> {
             List<VoxelCells> cells = new ArrayList<>();
-
             helper.readArray(buf, cells, buf2 -> {
                 short xSize = buf2.readUnsignedByte();
                 short ySize = buf2.readUnsignedByte();
                 short zSize = buf2.readUnsignedByte();
-
                 List<Short> storage = new ArrayList<>();
                 helper.readArray(buf2, storage, ByteBuf::readUnsignedByte);
-
                 return new VoxelCells(xSize, ySize, zSize, storage);
             });
-
             List<Float> xCoordinates = new ArrayList<>();
             helper.readArray(buf, xCoordinates, ByteBuf::readFloatLE);
-
             List<Float> yCoordinates = new ArrayList<>();
             helper.readArray(buf, yCoordinates, ByteBuf::readFloatLE);
-
             List<Float> zCoordinates = new ArrayList<>();
             helper.readArray(buf, zCoordinates, ByteBuf::readFloatLE);
-
             return new VoxelShape(cells, xCoordinates, yCoordinates, zCoordinates);
         });
-
         packet.setShapes(shapes);
-
         Map<String, Integer> nameMap = new HashMap<>();
-
         int size = VarInts.readUnsignedInt(buffer);
         for (int i = 0; i < size; i++) {
             nameMap.put(helper.readString(buffer), buffer.readUnsignedShortLE());
         }
-
         packet.setNameMap(nameMap);
     }
 }
